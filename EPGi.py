918c4d35dd5e3b1e03002b1bf6e2c7227bfd5539
Igor <igor469@gmail.com>
2025-08-04 19:34:02 +0300

    Add files via upload

--- EPGi.py
+++ EPGi.py
@@ -1,7 +1,2 @@
-#!/usr/bin/env python3
-"""Simple EPG interactive viewer.
+# -*- coding: utf-8 -*-
 
-This program reads configuration from EPGi.ini, downloads an EPG
-XML file (optionally gzipped), parses it and presents an
-interactive interface for browsing currently running TV shows.
-"""
